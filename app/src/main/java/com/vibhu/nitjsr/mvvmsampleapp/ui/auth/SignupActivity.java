b68a8e1f59ab633e1c5c80ec86e37500b64f9692
Joaquin144 <vvpp898.comco@gmail.com>
2022-01-03 02:30:31 +0530

    Auth UI done

---
+++ app/src/main/java/com/vibhu/nitjsr/mvvmsampleapp/ui/auth/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.vibhu.nitjsr.mvvmsampleapp.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vibhu.nitjsr.mvvmsampleapp.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
